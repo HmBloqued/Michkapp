9124b6199fe3c87303d127fce5ef5a7ff9a24bb0
Victor Masseron <victor.masseron--lucianaz@ig2i.centralelille.fr>
2024-06-20 16:52:12 +0200

    fix(pom): Update pom.xml

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -17,8 +17,13 @@ public class App extends Application implements Initializable {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("views/landing_page/landing_page.fxml"));
-        root.getStylesheets().add(getClass().getResource("views/landing_page/styles.css").toExternalForm());
+
+
+        System.out.println("1 = " + getClass().getResource("/views/landing_page/landing_page.fxml"));
+        System.out.println("2 = " + getClass().getResource("/views/property_selection/property_selection.fxml"));
+
+        Parent root = FXMLLoader.load(getClass().getResource("/views/landing_page/landing_page.fxml"));
+        root.getStylesheets().add(getClass().getResource("/views/landing_page/styles.css").toExternalForm());
         Scene scene = new Scene(root);
         stage.setTitle("Michk'App");
         stage.setScene(scene);
